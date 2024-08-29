ed5e49bc9710b798b732fde1d8195aa7026d7560
Talha <hpstar679@gmail.com>
2024-08-29 15:33:08 +0530

    chage

--- server/index.js
+++ server/index.js
@@ -26,6 +26,14 @@ const sessionConfig = {
   secret: "secret",
   resave: false,
   saveUninitialized: false,
+  store: store,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    domain: undefined,
+  },
 };
 
 app.use(session(sessionConfig));
